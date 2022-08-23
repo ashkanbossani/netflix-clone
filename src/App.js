370ddb92522fdbdcb1e49f1b6ad843a76e0d4101
ashkanbossani <abossani@hotmail.com>
2022-08-23 15:05:41 -0700

    created Row component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
   <>
+  <Row title="NETFLIX ORIGINALS"/>
+  <Row title="Trending Now"/>
+  <Row title="Top Rated"/>
+  <Row title="Action Movies"/>
+  <Row title="Comedy Movies"/>
+  <Row title="Horror Movies"/>
+  <Row title="Romance Movies"/>
+  <Row title="Documentaries"/>
   </>
   );
 }
