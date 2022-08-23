46e11da6a16a352ec7cb5cc0fa18346c6696864b
ashkanbossani <abossani@hotmail.com>
2022-08-23 16:13:36 -0700

    added all rows for diffrent genres and global styling to remove margins

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Row from './components/Row/Row';
 import requests from './requests';
+import './App.scss';
 
 function App() {
   return (
   <>
   <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
   <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-  {/* <Row title="Top Rated"/>
-  <Row title="Action Movies"/>
-  <Row title="Comedy Movies"/>
-  <Row title="Horror Movies"/>
-  <Row title="Romance Movies"/>
-  <Row title="Documentaries"/> */}
+  <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+  <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+  <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+  <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+  <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+  <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
   </>
   );
 }
