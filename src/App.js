7f3e8c2d7d11cb3bbcf12b35957abe229d05bf1e
ashkanbossani <abossani@hotmail.com>
2022-08-24 12:23:39 -0700

    added logo for login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const user = null 
   return (
     <>
       <div className="app">
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import "./Login.scss"
+import React from "react";
+import "./Login.scss";
 
 function Login(props) {
-    return (
-        <div className="login">
-            
-        </div>
-    );
+  return (
+    <div className="login">
+      <div className="login__background">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="login-logo"
+        />
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
