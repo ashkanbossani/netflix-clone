fffa8b20935a05a5793a4094207279656ec125d6
ashkanbossani <abossani@hotmail.com>
2022-08-23 19:26:38 -0700

    added nav bar animation to ease in when scrolling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Navbar.scss";
 
 function Navbar() {
 
+    const [show, handleShow] = useState(false);
+
     useEffect(() => {
-        
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 100) {
+                handleShow(true)
+            }else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener("scroll", () => {});
+        }
     } , []);
 
     return (
-        <div className="navbar">
+        <div className={`navbar ${show && "navbar__black"}`}>
             <img className="navbar__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" />
             <img className="navbar__avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix Avatar" />
             
