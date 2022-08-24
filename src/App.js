1cc7a0ecd58baead78cbaf1426aa0178303481f2
ashkanbossani <abossani@hotmail.com>
2022-08-24 12:12:29 -0700

    finished final refactor made app page hold diffrent page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import HomePage from "./pages/HomePage/HomePage";
+import Login from "./pages/Login/Login";
 import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="app">
-        <HomePage />
+        <Router>
+          {!user ? (
+            <Login />
+          ) : (
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+            </Switch>
+          )}
+        </Router>
       </div>
     </>
   );
