801a33dea5e849c7d8ae5149203ed3473412746e
ashkanbossani <abossani@hotmail.com>
2022-08-24 14:59:12 -0700

    finished sign in screen styling added sign up now

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./SignIn.scss";
 
 function SignIn() {
-    return (
-        <div className ="signin">
-            <form>
-                <h1>Sign In</h1>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button type="submit" className="signin__button">Sign In</button>
-                <h4></h4>
-            </form>
-        </div>
-    );
+  return (
+    <div className="signin">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" className="signin__button">
+          Sign In
+        </button>
+        <h4>
+          <span className="signin--grey">New to Netflix? </span>
+          <span className="signin--link">Sign up Now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
