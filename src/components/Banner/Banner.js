b329a4e929697a6ca7ce7576dc2a6ba94e2c541a
ashkanbossani <abossani@hotmail.com>
2022-08-23 18:02:27 -0700

    added styling for banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import requests from "../../requests";
-
+import "./Banner.scss";
 function Banner(props) {
   const [movie, setMovie] = useState([]);
 
@@ -18,6 +18,11 @@ function Banner(props) {
     fetchData();
   }, []);
 
+ 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -28,13 +33,16 @@ function Banner(props) {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie.title || movie.name || movie.original_name}</h1>
+        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
         <div className="banner__buttons">
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie.overview}</h1>
+        <h1 className="banner__description">{movie.overview}
+        {truncate(movie.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"/>
     </header>
   );
 }
