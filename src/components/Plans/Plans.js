eb95ee3f5d1724ec31d95f21c925b07440eb8b07
ashkanbossani <abossani@hotmail.com>
2022-08-25 18:21:10 -0700

    used use effect to pull data from firebase including the subscription and prices

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { db } from "../../firebase";
 import "./Plans.scss";
 
 function Plans() {
-    const  [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    
-    return (
-        <div className="plans">
-            
-        </div>
-    );
+  useEffect(() => {
+    db.collection("products")
+      .where("active", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection("prices").get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data(),
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+
+  console.log(products);
+
+  return <div className="plans">
+        {Object.entries(products).map(([productId, productData])=> {
+            return(
+                <div className="plans__plans">
+                    <div className="plans__info">
+                       <h5>{productData.name}</h5>
+                          <h6>{productData.description}</h6>
+                    </div>   
+                    <button>
+                        Subscribe
+                    </button>
+                </div>
+            )
+        }
+        )}
+  </div>;
 }
 
 export default Plans;
