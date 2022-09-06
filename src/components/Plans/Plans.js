8c6f8efbb149cb23061256da3ccf6e16a123d898
ashkanbossani <abossani@hotmail.com>
2022-09-05 17:44:36 -0700

    added load stripe with test publishable key and redirect to stripe checkout

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 import { db } from "../../firebase";
+import { loadStripe } from "@stripe/stripe-js";
 import "./Plans.scss";
 
 function Plans() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     db.collection("products")
@@ -25,7 +29,35 @@ function Plans() {
       });
   }, []);
 
-  console.log(products);
+  const handleLoadCheckout = async (priceId) => {
+    const docRef = await db.collection(
+      "customers".doc(user.uid).collection("checkout_session").add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      })
+    );
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+
+      if (error) {
+        //Show an error to your customer and
+        //   inspect Cloud Function logs in the Firebase console
+
+        //change this to pop up message
+        alert(`An error occurred: ${error.message}`);
+      }
+
+      if (sessionId){
+        //We have a session, let's redirect to Checkout
+        //Init Stripe
+        const stripe = await loadStripe(
+          "pk_test_51Lan96BvGQVk9GwsXqUft50qwct1JJvFgnCL423MLQKf5SLdm2MaNYfqLHrU16cl0ypgue9bgTxrzOeltynWHERd00ONyyxbvz"
+        );
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
 
   return (
     <div className="plans">
@@ -36,7 +68,11 @@ function Plans() {
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
-            <button>Subscribe</button>
+            <button
+              onClick={() => handleLoadCheckout(productData.prices.priceId)}
+            >
+              Subscribe
+            </button>
           </div>
         );
       })}
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -6,7 +6,7 @@ function SignIn() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const register = (e) => {
+  const handleRegister = (e) => {
     e.preventDefault();
 
     auth.createUserWithEmailAndPassword(
@@ -20,7 +20,7 @@ function SignIn() {
     );
   };
 
-  const login = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
     auth.signInWithEmailAndPassword(
@@ -40,12 +40,12 @@ function SignIn() {
         <h1>Sign In</h1>
         <input ref={emailRef} type="email" placeholder="Email" />
         <input ref={passwordRef} type="password" placeholder="Password" />
-        <button onClick={login} type="submit" className="signin__button">
+        <button onClick={handleLogin} type="submit" className="signin__button">
           Sign In
         </button>
         <h4>
           <span className="signin--grey">New to Netflix? </span>
-          <span onClick={register} className="signin--link">Sign Up now.</span>
+          <span onClick={handleRegister} className="signin--link">Sign Up now.</span>
         </h4>
       </form>
     </div>
